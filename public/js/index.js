9d2eb21c03ff582044f716dc5c66558d88a65fe8
ra5ika27 <31758298+ra5ika27@users.noreply.github.com>
2018-06-27 16:05:13 -0700

    Added jQuery and forms for messages

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,11 @@
 var socket = io();
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Rasika',
-    text: 'Hey'
-  });
+  //
+  // socket.emit('createMessage', {
+  //   from: 'Rasika',
+  //   text: 'Hey'
+  // });
 });
 
 socket.on('disconnect', function () {
@@ -18,4 +18,20 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   console.log('newMessage', message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from} : ${message.text}`);
+
+  jQuery('#messages').append(li);
+});
+
+//call back function for acknowledgement
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function () {
+
+  });
 });
