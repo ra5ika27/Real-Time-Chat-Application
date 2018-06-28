eebb509bdebcee0f6998d8f2124f370e138e0dca
ra5ika27 <31758298+ra5ika27@users.noreply.github.com>
2018-06-27 17:26:22 -0700

    Css for chat page

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ io.on('connection',(socket) => {
   socket.on('createMessage', (message, callback) => {
     console.log('create message', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
-    callback('This is from the server');
+    callback();
     // socket.broadcast.emit('newMessage', {
     //   from: message.from,
     //   text: message.text,
